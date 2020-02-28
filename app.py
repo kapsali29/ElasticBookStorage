b6f445e712cbf05ec988135587bf00671e573f77
kapsali29 <kapsali29@gmail.com>
2020-02-28 11:23:07 +0200

    some changes in app

--- app.py
+++ app.py
@@ -15,4 +15,12 @@ def ask_elastic_storage():
     else:
         elastic_results = builder.command(action=data['action'], payload=data)
         json_results = builder.get_source(elastic_results)
+
+        if 'file_type' in data.keys():
+            builder.save_results(
+                results=json_results,
+                file_name=data['action'],
+                file_type=data['file_type']
+            )
+
         return jsonify(json_results)
